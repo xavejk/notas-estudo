1911c9b43ef08b6817bf43cea3f604e466015188
Ana X <xaavierwow@gmai.com>
2026-03-19 19:09:24 -0300

    notas

---
+++ exemplos.js
@@ -0,0 +1,7 @@
+function exercicio10() {
+    //o que for pedido
+}
+
+const buttonExercicio = document.getElementById("exercicio")
+buttonExercicio.addEventListener('click', () => { exercicio() })
+
